7fa00a518b79763ac0ac717a5480b485c35f68d9
pasindu vinsuka <nimsarapasindu1@gmail.com>
2024-08-11 11:27:33 +0530

    feat:add hero section video

--- devtrio-fe/components/Hero.tsx
+++ devtrio-fe/components/Hero.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { ReactElement, ReactNode, useEffect } from "react";
 import { BackgroundGradientAnimation } from "./ui/background-gradient-animation";
 import Image from "next/image";
 
@@ -7,20 +7,33 @@ export default function Hero() {
   const green = "143, 211, 166";
   const blue = "111, 188, 237";
   const indigo = "133, 160, 242";
+  useEffect(() => {
+    const handleInteraction = () => {
+      const video = document.getElementById("hero-video") as HTMLVideoElement;
+      if (video) {
+        video.play();
+        window.removeEventListener("click", handleInteraction);
+      }
+    };
+
+    window.addEventListener("click", handleInteraction);
+
+    return () => window.removeEventListener("click", handleInteraction);
+  }, []);
   return (
-    <div>
+    <div className="h-auto ">
       <BackgroundGradientAnimation
         gradientBackgroundStart={"rgb(248, 253, 251)"}
         gradientBackgroundEnd={"rgb(248, 253, 251)"}
         firstColor={green}
         secondColor={green}
-        thirdColor={blue}
+        thirdColor={blue} 
         fourthColor={green}
         fifthColor={indigo}
         pointerColor={indigo}
-        className="max-w-screen-2xl"
+        className="max-w-screen-2xl "
       >
-        <div className=" absolute z-50 inset-0 flex text-black px-4 pointer-events-none text-3xl text-center flex-col gap-6 ">
+        <div className=" absolute z-50 inset-0 flex text-black px-4  text-3xl text-center flex-col gap-6 ">
           <div className="flex flex-row">
             <Image
               src={"/svgs/DevTrioLogo.svg"}
@@ -29,14 +42,36 @@ export default function Hero() {
               height={50}
             />
           </div>
-          <div className="pt-48">
-            <p className=" drop-shadow-2xl text-black md:text-4xl lg:text-7xl font-bold">
+          <div className="pt-48 ">
+            <p className=" drop-shadow-2xl text-black md:text-4xl lg:text-7xl font-bold pointer-events-none">
               Transforming Digital Visions into Reality
             </p>
-            <p className=" text-lg text-center pt-4">
+            <p className=" text-lg text-center pt-4 pointer-events-none">
               Helping business owners and start-ups gain an unfair advantage
               through industry-leading, results-driven marketing websites
             </p>
+            {/* Video Background */}
+
+            <div className="items-center flex flex-col mt-20">
+              <video
+                id="hero-video"
+                controls
+                preload="none"
+                className="w-3/4 max-w-full h-auto rounded-xl"
+                autoPlay
+                muted={false}
+                loop
+              >
+                <source src="/videos/intro.mp4" type="video/mp4" />
+                <track
+                  src="/path/to/captions.vtt"
+                  kind="subtitles"
+                  srcLang="en"
+                  label="English"
+                />
+                Your browser does not support the video tag.
+              </video>
+            </div>
           </div>
         </div>
       </BackgroundGradientAnimation>
